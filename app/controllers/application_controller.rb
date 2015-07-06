bab3e1c53129d073009a434ab4bfb8f2acbbea98
Wen-Tien Chang <ihower@gmail.com>
2015-07-06 12:42:21 +0800

    log current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_action :log_current_user
 
   protected
+
+  def log_current_user
+    if current_user
+      Rails.logger.info "current_user_id: #{current_user.id}"
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :first_name
