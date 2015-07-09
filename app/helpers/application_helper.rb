46952345d5cbeb79742a515517a78c42a8e1aaa9
Wen-Tien Chang <ihower@gmail.com>
2015-07-09 11:30:20 +0800

    Add gravatar_url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+    def gravatar_url(email)
+      gravatar_email = Digest::MD5.hexdigest(email.downcase)
+      return "http://www.gravatar.com/avatar/#{gravatar_email}?s=48"
+    end
+
 end
